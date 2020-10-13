a3ee0781011c001e25287aec6278fc2486f6db6f
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2020-10-13 14:26:41 +0200

    added DisplayNameLocalized attribute

---
+++ Attributes/DisplayNameLocalizedAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using Common.Shared.Min.Extensions;
+
+namespace Common.Shared.Min.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Event | AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Enum)]
+	public class DisplayNameAttributeLocalized : DisplayNameAttribute
+	{
+		public DisplayNameAttributeLocalized(string displayNameKey, Type resourceType)
+			: base(displayNameKey)
+		{
+			resourceType.ThrowIfNull(nameof(resourceType));
+
+			var nameProperty = resourceType!.GetProperty(base.DisplayName,
+				BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+			if (nameProperty is null)
+				return;
+
+			DisplayNameValue = (string)nameProperty.GetValue(nameProperty.DeclaringType, null)!;
+		}
+	}
+}
